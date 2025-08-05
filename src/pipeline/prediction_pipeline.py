91f44b0da311041f41ddb4d7e7fbd70e82f2cb9e
Aiswarya-EDSTEM <aiswarya.babu@edstem.com>
2025-08-05 14:06:28 +0530

    Deployement configurations

---
+++ src/pipeline/prediction_pipeline.py
@@ -0,0 +1,120 @@
+    # import sys
+    # import pandas as pd
+    # from src.exception import CustomException
+    # from src.utils import load_object
+
+
+    # class PredictPipeline:
+    #     def __init__(self):
+    #         pass
+
+    #     def predict(self,features):
+    #         try:
+    #             model_path-"artifacts\model.pkl"
+    #             preprocessor_path="artifacts\preprocessor.pkl"
+    #             model=load_object(file_path=model_path)
+    #             preprocessor=load_object(file_path=preprocessor_path)
+    #             data_scaled=preprocessor.transform(features)
+    #             preds=model.predict(data_scaled)
+    #             return preds
+    #         except Exception as e:
+    #             raise CustomException(e,sys)
+
+    # class CustomData:
+    #     def __init__( self,
+    #         gender:str,
+    #         race/ethinicity: str,
+    #         parental level of education,
+    #         lunch: str,
+    #         test preparation course: str,
+    #         reading score: int,
+    #         writing score: int,):
+
+    #         self.gender = gender
+    #         self.race/ethnicity = race/ethnicity
+    #         self.parental level of education = parental level of education
+    #         self.lunch = lunch
+    #         self.test preparation course = test preparation course
+    #         self.reading score = reading score
+    #         self.writing score = writing score
+
+
+    # def get_data_as_data_frame(self):
+    #     try:
+    #         custom_data_input_dict={
+    #             "gender": [self.gender],
+    #             "race/ethnicity": [self.race/ethnicity],
+    #             "parental level of education": [self.parental level of education],
+    #             "lunch": [self.lunch],
+    #             "test preparation course":[self.test preparation course],
+    #             "reading score": [self.reading score],
+    #             "writing score": [self.writing score],
+    #         }
+    #         return pd.DataFrame(Custom_data_input_dict)
+    #     except Exception as e:
+    #         raise CustomException(e,sys)
+
+
+import sys
+import pandas as pd
+from src.exception import CustomException
+from src.utils import load_object
+
+
+class PredictPipeline:
+    def __init__(self):
+        pass
+
+    def predict(self, features):
+        try:
+            model_path = "artifacts/model.pkl"  # Use = instead of -
+            preprocessor_path = "artifacts/preprocessor.pkl"
+            
+            model = load_object(file_path=model_path)
+            preprocessor = load_object(file_path=preprocessor_path)
+            
+            data_scaled = preprocessor.transform(features)
+            preds = model.predict(data_scaled)
+            return preds
+
+        except Exception as e:
+            raise CustomException(e, sys)
+
+
+class CustomData:
+    def __init__(
+        self,
+        gender: str,
+        race_ethnicity: str,
+        parental_level_of_education: str,
+        lunch: str,
+        test_preparation_course: str,
+        reading_score: int,
+        writing_score: int
+    ):
+        self.gender = gender
+        self.race_ethnicity = race_ethnicity
+        self.parental_level_of_education = parental_level_of_education
+        self.lunch = lunch
+        self.test_preparation_course = test_preparation_course
+        self.reading_score = reading_score
+        self.writing_score = writing_score
+
+    def get_data_as_data_frame(self):
+        try:
+            custom_data_input_dict = {
+                "gender": [self.gender],
+                "race/ethnicity": [self.race_ethnicity],
+                "parental level of education": [self.parental_level_of_education],
+                "lunch": [self.lunch],
+                "test preparation course": [self.test_preparation_course],
+                "reading score": [self.reading_score],
+                "writing score": [self.writing_score],
+            }
+
+            return pd.DataFrame(custom_data_input_dict)
+
+        except Exception as e:
+            raise CustomException(e, sys)
+
+
